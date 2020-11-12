11344910c3c9a3b16bc2c318ba1d30e6118200bf
Ramakrishna Bobba <ramkrishnabobba@gmail.com>
2020-11-12 00:44:51 -0600

    selection sort

--- selection-sort/Program.cs
+++ selection-sort/Program.cs
@@ -6,7 +6,35 @@ namespace selection_sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var input = new int[] {5,2,3,1};
+            var input = new int[] {5,1,1,2,0,0};
+            var output = Sort(input);
+            foreach(int val in output)
+            {
+                Console.WriteLine(val);
+            }
+        }
+
+        private static int[] Sort(int[] input)
+        {
+            if(input.Length <= 1)
+                return input;
+            int temp,min;
+            for(int i = 0; i < input.Length-1; i++)
+            {
+                min = i;
+                for(int j = i+1; j < input.Length; j++)
+                {
+                    if(input[i] > input[j])
+                    {
+                        min = j;
+                    }
+                }
+                temp = input[i];
+                input[i] = input[min];
+                input[min] = temp;
+            }
+            return input;
         }
     }
 }
